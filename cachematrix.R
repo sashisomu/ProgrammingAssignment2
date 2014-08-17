c73ddbfc27df8ab349550ab121c10d7178cd823f
sashisomu <sashisomu@gmail.com>
2014-08-17 19:42:51 +0530

    modified sceletion cachematrix as per assignment requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function to is to crate a matrix object to cache its inverse
+## Takes a invertible matrix as input
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    setsolve <- function(solve) {
+        m <<- solve
+    }
+    getsolve <- function() {
+        m
+    }
+    
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Checks for inverse of the matrix in cache and returns it
+## if inverse doesn't exist Calculates the inverse of the matrix and returns it 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getsolve()
+    
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data,...)
+    x$setsolve(m)
+    m
+    
 }
